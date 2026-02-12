0aaa4d8de07b6a9eeeb23eff44c731838bcb580f
LucaO05 <LucaOestreich2804@gmail.com>
2026-02-12 10:37:11 +0100

    add folder structure & prisma init

---
+++ mini_facebook/app/layout.tsx
@@ -0,0 +1,19 @@
+import type {Metadata} from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "Mini Facebook",
+    description: "the only good Social Media Plattform",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="de">
+        <body className="antialiased">{children}</body>
+        </html>
+    );
+}
